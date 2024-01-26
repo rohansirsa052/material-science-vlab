84dc4e3780a1f8cb7e3af8f128b87c2603bc56bd
Abhineet Verma <198.abhineet@gmail.com>
2024-01-26 18:20:45 +0530

    Ques update, added furnance temp

--- experiments/6/js/index.js
+++ experiments/6/js/index.js
@@ -1,108 +1,22 @@
 const charts = {};
 const schema = ["elongation", "time"];
-const readingData1 = [
-  [0, 0.38],
-  [3, 0.45],
-  [9, 0.5],
-  [15, 0.52],
-  [21, 0.53],
-  [27, 0.56],
-  [33, 0.58],
-  [39, 0.59],
-  [45, 0.61],
-  [50, 0.62],
-  [56, 0.64],
-  [62, 0.65],
-  [68, 0.66],
-  [75, 0.67],
-  [80, 0.69],
-  [86, 0.7],
-  [92, 0.72],
-  [98, 0.74],
-  [104, 0.76],
-  [110, 0.8],
-  [116, 0.86],
-  [119, 0.9],
-  [121, 0.96],
-  [123, 1.03],
-  [126, 1.04],
-  [128, 1.33],
-];
 
-const readingData2 = [
-  [0, 0.41],
-  [5, 0.5],
-  [10, 0.57],
-  [16, 0.61],
-  [22, 0.65],
-  [28, 0.68],
-  [34, 0.71],
-  [40, 0.75],
-  [45, 0.79],
-  [51, 0.83],
-  [58, 0.86],
-  [63, 0.9],
-  [70, 0.94],
-  [75, 0.98],
-  [81, 1.03],
-  [87, 1.1],
-  [89, 1.1],
-  [94, 1.21],
-  [97, 1.24],
-  [100, 1.28],
-  [103, 1.38],
-];
-const readingData3 = [
-  [0, 0.87],
-  [0.5, 0.75],
-  [1, 0.92],
-  [1.5, 0.83],
-  [1.6, 1.01],
-  [3.5, 1.06],
-  [5.39, 1.11],
-  [7, 1.15],
-  [9, 1.19],
-  [10, 1.23],
-  [12, 1.31],
-  [14, 1.36],
-  [15, 1.45],
-  [16, 1.59],
-];
+const readingData1 = [[0.01,0.38],[0.89,0.43],[3.37,0.45],[6.07,0.48],[9.19,0.5],[12.17,0.51],[15.15,0.52],[18.12,0.53],[21.1,0.54],[24.08,0.55],[27.05,0.56],[30.03,0.57],[33.01,0.58],[35.98,0.58],[38.96,0.59],[41.94,0.6],[44.91,0.61],[47.89,0.62],[50.87,0.62],[53.84,0.63],[56.82,0.64],[59.8,0.64],[62.77,0.65],[65.75,0.66],[68.73,0.66],[71.7,0.67],[74.68,0.67],[77.66,0.68],[80.63,0.69],[83.61,0.7],[86.59,0.7],[89.56,0.71],[92.54,0.72],[95.52,0.73],[98.49,0.74],[101.47,0.74],[104.45,0.76],[107.42,0.78],[110.33,0.8],[113.58,0.83],[116.15,0.86],[118.5,0.9],[119.4,0.9],[121.14,0.94],[121.48,0.96],[123.84,1.03],[123.84,1.03],[126.73,1.1],[126.23,1.04],[127.4,1.16],[128.95,1.33]];
+
+const readingData2 = [[0.01,0.41],[2.71,0.45],[4.94,0.5],[7.13,0.54],[10.16,0.57],[13.16,0.6],[16.27,0.61],[19.12,0.63],[22.09,0.65],[25.07,0.67],[28.05,0.68],[31.02,0.7],[34,0.71],[36.97,0.73],[39.9,0.75],[42.93,0.78],[45.84,0.79],[48.88,0.81],[51.86,0.83],[54.93,0.84],[57.81,0.86],[60.79,0.88],[63.76,0.9],[66.93,0.92],[69.65,0.94],[72.69,0.96],[75.73,0.98],[78.78,1.01],[81.45,1.03],[84.6,1.06],[87.18,1.1],[89.71,1.14],[89.89,1.1],[92.54,1.16],[94.78,1.21],[95.52,1.18],[97.62,1.24],[97.83,1.19],[99.99,1.28],[101.29,1.32],[102.7,1.38],[104.03,1.45]];
+
+const readingData3 = [[0.01,0.87],[0.02,0.78],[0.03,0.75],[1.12,0.92],[1.3,0.83],[1.61,1.01],[1.52,0.97],[3.57,1.06],[5.39,1.11],[7.21,1.15],[9.16,1.19],[10.7,1.24],[12.48,1.31],[12.17,1.27],[13.91,1.36],[15.08,1.45],[14.55,1.4],[15.81,1.53],[14.82,1.49],[16.14,1.59]];
 
 // x axis
-const time1 = [
-  0.01, 0.89, 3.37, 6.07, 9.19, 12.17, 15.15, 18.12, 21.1, 24.08, 27.05, 30.03, 33.01, 35.98, 38.96, 41.94, 44.91,
-  47.89, 50.87, 53.84, 56.82, 59.8, 62.77, 65.75, 68.73, 71.7, 74.68, 77.66, 80.63, 83.61, 86.59, 89.56, 92.54, 95.52,
-  98.49, 101.47, 104.45, 107.42, 110.33, 113.58, 116.15, 118.5, 119.4, 121.14, 121.48, 123.84, 123.84, 126.73, 126.23,
-  127.4, 128.95,
-];
+const time1 = readingData1.map((x) => x[0]);
 // y axis
-const elongation1 = [
-  0.38, 0.43, 0.45, 0.48, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.58, 0.59, 0.6, 0.61, 0.62, 0.62, 0.63,
-  0.64, 0.64, 0.65, 0.66, 0.66, 0.68, 0.67, 0.68, 0.69, 0.7, 0.7, 0.71, 0.72, 0.73, 0.74, 0.74, 0.76, 0.78, 0.8, 0.83,
-  0.86, 0.9, 0.9, 0.94, 0.96, 1.03, 1.03, 1.1, 1.04, 1.16, 1.33,
-];
+const elongation1 = readingData1.map((x) => x[1]);
 
-const time2 = [
-  0.01, 2.71, 4.94, 7.13, 10.16, 13.16, 16.27, 19.12, 22.09, 25.07, 28.05, 31.02, 34.0, 36.97, 39.9, 42.93, 45.84,
-  48.88, 51.86, 54.93, 57.81, 60.79, 63.76, 66.93, 69.65, 72.69, 75.73, 78.78, 81.45, 84.6, 87.18, 89.71, 89.89, 92.54,
-  94.78, 95.52, 97.62, 97.83, 99.99, 101.29, 102.7, 104.03,
-];
+const time2 = readingData2.map((x) => x[0]);
+const elongation2 = readingData2.map((x) => x[1]);
 
-const elongation2 = [
-  0.41, 0.45, 0.5, 0.54, 0.57, 0.6, 0.61, 0.63, 0.65, 0.67, 0.68, 0.7, 0.71, 0.73, 0.75, 0.78, 0.79, 0.81, 0.83, 0.84,
-  0.86, 0.88, 0.9, 0.92, 0.94, 0.96, 0.98, 1.01, 1.03, 1.06, 1.1, 1.14, 1.1, 1.16, 1.21, 1.18, 1.24, 1.19, 1.28, 1.32,
-  1.38, 1.45,
-];
-
-const time3 = [
-  0.01, 0.02, 0.03, 1.12, 1.3, 1.61, 1.52, 3.57, 5.39, 7.21, 9.16, 10.7, 12.48, 12.17, 13.91, 15.08, 14.55, 15.81,
-  14.82, 16.13,
-];
-
-const elongation3 = [
-  0.87, 0.78, 0.75, 0.92, 0.83, 1.01, 0.97, 1.06, 1.11, 1.15, 1.18, 1.24, 1.31, 1.27, 1.36, 1.45, 1.4, 1.53, 1.49, 1.59,
-];
+const time3 = readingData3.map((x) => x[0]);
+const elongation3 = readingData3.map((x) => x[1]);
 
 var currPos = 0;
 
@@ -352,12 +266,21 @@ function handleStep3() {
 
         if (currPos >= readingData3.length) {
           clearInterval(intr3);
-          document.getElementById("startTest").disabled = false;
+          // document.getElementById("startTest").disabled = false;
           document.getElementById("startTest").innerHTML = "Done";
           // document.getElementById("showGraphBtn").disabled = false;
           utm.stop();
           document.getElementById("btnNext").disabled = false;
           // document.getElementById("arrowNext").classList.remove("disabled");
+          let cnt = document.getElementById("furnaceTemp");
+          let count = 800;
+          let intr = setInterval(() => {
+            if (count < 25) {
+              clearInterval(intr);
+              return;
+            }
+            cnt.innerHTML = count--;
+           }, 30);
           return;
         }
 
@@ -410,6 +333,19 @@ function handleStep3() {
       }, 500);
     }
   });
+
+  document.getElementById("startTest").innerHTML = "Heating up...";
+  let cnt = document.getElementById("furnaceTemp");
+  let count = 25;
+  let intr = setInterval(() => {
+    if (count > 800) {
+      clearInterval(intr);
+      document.getElementById("startTest").innerHTML = "Start Test";
+      document.getElementById("startTest").disabled = false;
+      return;
+    }
+    cnt.innerHTML = count++;
+   }, 10);
 
   pane.classList.add("done");
   pane.classList.remove("active");
@@ -471,37 +407,71 @@ function handleStep5() {
     title: "Can you answer the questions?",
     body: [
       {
-        page: 1,
-        title: "Creep depends on temperature?",
-        //image: "images/creep test curve.png",
-        options: ["True", "False"],
-        correct: 0,
+        "page": 1,
+        "title": "In which stage, the creep rate is constant?",
+        "options": ["Primary", "Secondary", "Tertiary", "It is not constant in all the three stages"],
+        "correct": 1
       },
       {
-        page: 2,
-        title: "In which of the stages, do we observe a constant deformation rate?",
-        //image: "images/creep test curve.png",
-        options: ["Transient creep stage", "Constant creep stage", "Fracture stage", "Steady stage creep stage"],
-        correct: 3,
+        "page": 2,
+        "title": "In which of the following tests load is generally kept constant?",
+        "options": ["Creep testing", "Compression Testing", "Impact Testing", "Tensile Testing"],
+        "correct": 0
       },
       {
-        page: 3,
-        title: "In which of the following stages do the deformation rate increases and causes failure?",
-        //image: "images/creep test curve.png",
-        options: ["Transient creep stage", "Constant creep stage", "Fracture stage", "Steady stage creep stage"],
-        correct: 2,
+        "page": 3,
+        "title": "It is given that aluminium can creep at and above 0.4 Tm, where Tm has its usual meaning. Calculate the minimum temperature (in oC) at which aluminium can creep? Melting point of aluminum is 660 oC",
+        "options": ["Room temperature", "264", "100.2", "660"],
+        "correct": 2
       },
       {
-        page: 4,
-        title: "Which of the following is true for Creep Test?",
-        options: [
-          "The slope of the strain-time graph increases with temperature and stress",
-          "The slope of strain-time graph decreases with temperature",
-          "The slope of strain-time graph decreases with stress",
-          "The slope of strain-time graph does not depend on temperature or stress",
-        ],
-        correct: 0,
+        "page": 4,
+        "title": "What is/are true for Stage III (tertiary stage) of creep?",
+        "options": ["The strain rate is the minimum in this stage", "This stage is of shorter duration than secondary stage", "The material deforms with a continuously decreasing creep rate", "The strain hardening rate balances the recovery rate"],
+        "correct": 1
       },
+      {
+        "page": 5,
+        "title": "In which stage the effect of strain hardening is more than softening?",
+        "options": ["Stage I", "Stage II", "Stage III", "Strain hardening is never higher in any stage"],
+        "correct": 0
+      },
+      {
+        "page": 6,
+        "title": "Creep rate in the tertiary stage",
+        "options": ["Increases", "Decreases", "Remains constant", "First increases and then decreases"],
+        "correct": 0
+      },
+      {
+        "page": 7,
+        "title": "It is given that copper can creep at and above 0.5 Tm, where Tm has its usual meaning. Calculate the minimum temperature (in oC) at which Copper can creep? Melting point of copper is 1084 oC",
+        "options": ["542", "405.5", "Room temperature", "1084"],
+        "correct": 1
+      },
+      {
+        "page": 8,
+        "title": "Void formation takes place in which stage?",
+        "options": ["Stage II", "Stage III", "Stage I", "When instantaneous strain develops"],
+        "correct": 1
+      },
+      {
+        "page": 9,
+        "title": "In which stage there is a balance between strain hardening and softening?",
+        "options": ["Stage III", "When instantaneous stain develops.", "Stage II", "Stage I"],
+        "correct": 2
+      },
+      {
+        "page": 10,
+        "title": "Which stage is of longest duration?",
+        "options": ["Stage I", "Duration where instantaneous strain develops", "Stage III", "Stage II"],
+        "correct": 3
+      },
+      {
+        "page": 11,
+        "title": "Time to start the tertiary creep stage __________ with increase in the temperature",
+        "options": ["Decrease", "Increases", "Remains constant", "It first decreases and then increases with time"],
+        "correct": 0
+      }
     ],
     onClose: () => {
       let pane = document.getElementById("step6");
